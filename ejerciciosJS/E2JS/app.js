5c9629cb58159766e0b5a35d88f48a82a5599425
ing-gamma77 <ing.gmacias@gmail.com>
2024-01-28 17:27:12 -0600

    Programa que multiplica dos numeros

---
+++ ejerciciosJS/E2JS/app.js
@@ -0,0 +1,8 @@
+let valor1;
+let valor2;
+
+alert('Programa que multiplica dos numeros diferentes');
+valor1 = prompt('Dame el primer numero a multiplicar')
+valor2 = prompt('Dame el segundo numero');
+alert(valor1*valor2);
+console.log (valor1 * valor2);
