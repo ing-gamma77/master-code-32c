21e6049256f3afa7d619d56270fe9b5a91aec4e0
ing-gamma77 <ing.gmacias@gmail.com>
2024-01-29 21:02:45 -0600

    Clase de arregos o arrays

---
+++ P5-ARREGLOS/app.js
@@ -0,0 +1,37 @@
+//ARRAYS
+
+/*let user = ['Gama', 'Macias', 47, '@gamaliel.macias', true];
+console.log(user)
+console.log(user[2]);
+
+user[2] = 48;
+console.log(user[2]);
+console.log(user);
+
+console.log(user.indexOf('@gamaliel.macias'));
+console.log(user.indexOf('rios'));
+*/
+
+/*let pokemon = ['pikachu', 'bulbasaur', 'charmander'];
+console.log(pokemon);
+
+let pokemonLength = pokemon.push('evee');
+console.log(pokemon);
+console.log(pokemonLength)
+
+pokemonLength = pokemon.unshift('snorlax');
+console.log(pokemon);
+console.log(pokemonLength);
+
+pokemon.pop();
+console.log(pokemon);
+
+pokemon.shift();
+console.log(pokemon);*/
+
+let string = 'pikachu, bulbasaur, charmander';
+let stringToArray = string.split('a');
+console.log(string)
+
+
+
