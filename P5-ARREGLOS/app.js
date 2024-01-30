108e67210e282817e19692cadbeeb56d96e9b292
ing-gamma77 <ing.gmacias@gmail.com>
2024-01-29 21:25:59 -0600

    mas ejemplos

--- P5-ARREGLOS/app.js
+++ P5-ARREGLOS/app.js
@@ -1,10 +1,31 @@
 //ARRAYS
-
-/*let user = ['Gama', 'Macias', 47, '@gamaliel.macias', true];
-console.log(user)
+let user = ['Gama', 'Macias', 'Rdz', '47', '2 perros' ,'@gamaliel.macias', 'true'];
+/*console.log(user[0]);
+console.log(user[1]);
 console.log(user[2]);
+console.log(user[3]);
+console.log(user[4]);
+console.log(user[5]);*/
+/*for(let i = 0; i <= 4; i++) {
+    console.log(user[i]);
+}*/
 
-user[2] = 48;
+console.log(user.length);
+for(let i = 0; i <= user.length -1; i++) {
+    console.log(user[i]);
+}
+
+console.log('------------------');
+
+for(let i = 0; i < user.length; i++) {
+    console.log(user[i]);
+}
+
+console.log('------------------');
+
+
+
+/*user[2] = 48;
 console.log(user[2]);
 console.log(user);
 
@@ -29,9 +50,10 @@ console.log(pokemon);
 pokemon.shift();
 console.log(pokemon);*/
 
-let string = 'pikachu, bulbasaur, charmander';
+/*let string = 'pikachu, bulbasaur, charmander';
 let stringToArray = string.split('a');
-console.log(string)
+console.log(string) */
+
 
 
 
